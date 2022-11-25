28e6c1809be52c6e728ffe4c4423a0bedbf108c8
Adelina Radulescu <adel0896@stud.kea.dk>
2022-11-25 22:10:48 +0100

    finished buttons with filters, prep for polygon

--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState, useMemo } from "react";
-import Map, { GeolocateControl, Marker, NavigationControl, FullscreenControl, ScaleControl } from "react-map-gl";
+import Map, { GeolocateControl, Marker, NavigationControl, FullscreenControl, ScaleControl, Layer } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { render } from "react-dom";
 import styles from "./css-modules/myMap.module.css";
@@ -7,9 +7,25 @@ import Button from "./Button";
 
 import Pin from "./Pin";
 import axios from "axios";
+
 function MyMap() {
   const [geojson, setGeoJson] = useState(null);
+  const pinRef = useRef();
   const mapRef = useRef();
+
+  const valbyLayer = {
+    id: "places",
+    type: "symbol",
+    source: {
+      type: "geojson",
+      data: geojson,
+    },
+    layout: {
+      "icon-image": "bar-15",
+      "icon-size": 1.25,
+      "icon-allow-overlap": true,
+    },
+  };
 
   useEffect(() => {
     axios.get("https://wfs-kbhkort.kk.dk/k101/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=k101:biblioteker&outputFormat=json&SRSNAME=EPSG:4326").then((response) => {
@@ -36,6 +52,7 @@ function MyMap() {
   function handleMove() {
     console.log("moving");
     if (!mapRef.current) return;
+
     setLng(mapRef.current.getCenter().lng.toFixed(4));
     setLat(mapRef.current.getCenter().lat.toFixed(4));
     setZoom(mapRef.current.getZoom().toFixed(2));
@@ -47,29 +64,48 @@ function MyMap() {
 
   useEffect(() => {
     if (!mapRef.current) return;
-    if (location === "Frederiksberg") {
+    if (location === "Valby") {
       mapRef.current.flyTo({
-        center: [12.5359, 55.6774],
+        center: [12.516775, 55.661659],
         essential: true,
-        zoom: 13, // this animation is considered essential with respect to prefers-reduced-motion
+        zoom: 13,
       });
-    } else if (location === "Copenhagen K") {
+    } else if (location === "København Ø") {
+      mapRef.current.flyTo({
+        center: [12.57517469858807, 55.70923313429004],
+        essential: true,
+        zoom: 13,
+      });
+    } else if (location === "All") {
+      mapRef.current.flyTo({
+        center: [lng, lat],
+        essential: true,
+        zoom: 10,
+      });
+    } else if (location === "Brønshøj") {
+      mapRef.current.flyTo({
+        center: [12.49056929207108, 55.70738055947126],
+        essential: true,
+        zoom: 13,
+      });
     }
   }, [location]);
 
   return (
     <div>
       <div className={styles.container}>
-        <Button text={"All"} />
-        <Button text={"Frederiksberg"} onClick={changeLocation} value="Frederiksberg" />
-        <Button text={"Copenhagen K"} />
-        <Button text={"Vanlose"} />
+        <Button text={"All"} onClick={changeLocation} value="All" />
+        <Button text={"Valby"} onClick={changeLocation} value="Valby" />
+        <Button text={"Østerbro"} onClick={changeLocation} value="København Ø" />
+        <Button text={"Brønshøj"} value="Brønshøj" onClick={changeLocation} />
       </div>
       <div className={styles.sidebar}>
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div>
 
       <Map
+        // style="bright-v8"
+        // onLoad={addSomeLayer}
         ref={mapRef}
         onMove={handleMove}
         initialViewState={{
@@ -78,7 +114,7 @@ function MyMap() {
           zoom: zoom,
         }}
         style={{ width: 800, height: 500 }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapStyle="mapbox://styles/mapbox/bright-v8"
         mapboxAccessToken={TOKEN}
       >
         <GeolocateControl position="top-left" />
@@ -86,6 +122,7 @@ function MyMap() {
         <NavigationControl position="top-left" />
         <ScaleControl />
         {pinsArray}
+        <Layer {...valbyLayer} />
       </Map>
     </div>
   );
